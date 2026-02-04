f27318e907e56885a41480d9ce27d1ead6018494
jeeva-muthamizh <mjeeva652@gamil.com>
2026-02-04 20:29:29 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,3 @@
+Organization = input("Enter your Organization name: ")
+Working_Experience = input("Enter your current working experience: ")
+print(f"He is working in {Organization} for {Working_Experience}")
